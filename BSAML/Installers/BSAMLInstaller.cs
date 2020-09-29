74835f042bbc47f9133504c9328f96db93da0700
Auros Nexus <aurosnex@gmail.com>
2020-09-28 23:36:31 -0400

    custom view controller to automatically handle parsing

--- BSAML/Installers/BSAMLInstaller.cs
+++ BSAML/Installers/BSAMLInstaller.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Knit;
+using System;
+using Serilog;
 using Zenject;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BSAML.Installers
 {
     internal class BSAMLInstaller : Installer<IServiceProvider, BSAMLInstaller>
     {
+        internal const string PROVIDER_ID = "BSAMLProvider";
+
         private readonly IServiceProvider serviceProvider;
 
         public BSAMLInstaller(IServiceProvider serviceProvider)
@@ -14,7 +19,25 @@ namespace BSAML.Installers
 
         public override void InstallBindings()
         {
-            Container.BindInstance(serviceProvider).WithId("BSAMLProvider").AsSingle();    
+            Container.BindInstance(serviceProvider).WithId(PROVIDER_ID).AsSingle();
+            Container.Bind<DynamicParser>().FromFactory<ParserFactory>();
+        }
+    }
+
+    internal class ParserFactory : IFactory<DynamicParser>
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public ParserFactory([Inject(Id = BSAMLInstaller.PROVIDER_ID)] IServiceProvider provider) => serviceProvider = provider;
+
+        public DynamicParser Create()
+        {
+            var parser = new DynamicParser(
+                    serviceProvider.GetRequiredService<IXamlReaderProvider>(),
+                    serviceProvider.GetRequiredService<ILogger>(),
+                    serviceProvider
+                );
+            return parser;
         }
     }
 }
---
+++ BSAML/ViewControllers/PanelViewController.cs
@@ -0,0 +1,51 @@
+﻿using HMUI;
+using Zenject;
+using UnityEngine;
+using BSAML.Elements;
+using System.Collections;
+using IPA.Utilities.Async;
+
+namespace BSAML.ViewControllers
+{
+    public abstract class PanelViewController : ViewController
+    {
+        [Inject] protected internal DynamicParser parser = null!;
+
+        public abstract string? XAML { get; }
+        public ViewPanel Panel { get; set; } = null!;
+
+        protected override void DidActivate(bool firstActivation, ActivationType activationType)
+        {
+            base.DidActivate(firstActivation, activationType);
+            Panel = (ViewPanel)parser.ParseXaml(XAML is null ? DefaultPanel : XAML); // An error page can also be rendered here if the parse fails.
+            Panel.DataContext = this;
+            var width = rectTransform.rect.width;
+            var height = rectTransform.rect.height;
+            StartCoroutine(RenderAndParent(width, height));
+        }
+
+        private IEnumerator RenderAndParent(float w, float h)
+        {
+            var renderTask = Panel.Render(new LayoutInformation(w, h));
+
+            yield return Coroutines.WaitForTask(renderTask);
+
+            var obj = renderTask.Result;
+
+            var rt = obj.GetComponent<RectTransform>();
+            rt.SetParent(rectTransform, false);
+            rt.sizeDelta = Vector2.zero;
+            rt.anchorMin = Vector2.zero;
+            rt.anchorMax = Vector2.one;
+            rt.anchoredPosition = Vector2.zero;
+        }
+
+        const string DefaultPanel = @"
+<ViewPanel xmlns=""bsaml""
+           xmlns:k=""knit""
+           xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">
+    <Text Value=""No Content"" />
+</ViewPanel>
+";
+    }
+}
